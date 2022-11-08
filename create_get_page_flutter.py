bcc86e7808bb38858767e0dcf1509d39cce381dd
HarryKael <harrykael1@gmail.com>
2022-11-08 09:24:10 -0400

    Creating the get_path function
    With this function I get the name of the page and create the directory
    with the name introduced with '_page' at the end of the directory name.

--- create_get_page_flutter.py
+++ create_get_page_flutter.py
@@ -6,45 +6,19 @@ import subprocess
 
 
 def get_paths():
+    """Get the data to make the path for the elements of the page."""
     try:
+        # Get the name introduce by the terminal.
         name = sys.argv[1]
+        # Print the name of the page and the name that will have the directory of the page.
         print("Nombre: {name}, Path: {name}_page".format(name=name))
+        # If the directory does not exist create one.
         if not os.path.exists("{}_page".format(name)):
             subprocess.run(['mkdir', "{}_page".format(name)])
+        # Return the name of the page and the name of the directory
         return name, "{}_page".format(name)
     except IndexError as e:
+        # When a error happend finish the command, with the error message on the screen."
         print("Error: {}".format(e))
         sys.exit(1)
 
-
-def create(name: str, path: str, path_placeholder: str, tipo: str):
-    pattern = r"Placeholder"
-    pattern2 = r"placeholder_"
-    full_path = "{path}/{name}_{type}.dart".format(path=path, name=name, type=tipo)
-    if not os.path.exists(full_path):
-        rows = []
-        with open(path_placeholder, 'r') as placeholder:
-            for line in placeholder:
-                if re.search(pattern2, line):
-                    rows.append(re.sub(pattern2, '{}_'.format(name.lower()), line))
-                else:
-                    rows.append(re.sub(pattern, name.capitalize(), line))
-            placeholder.close()
-        with open(full_path, 'w') as file:
-            file.writelines(rows)
-            file.close()
-
-
-def main():
-    """Automation of the creation of one page in flutter using the State Managment with 'GET'"""
-    placeholder_controller = "/Users/macbook/Documents/Harry/commands/placeholders/lib/src/placeholder_controller.dart"
-    placeholder_binding = "/Users/macbook/Documents/Harry/commands/placeholders/lib/src/placeholder_binding.dart"
-    placeholder_page = '/Users/macbook/Documents/Harry/commands/placeholders/lib/src/placeholder_page.dart'
-    name, path = get_paths()
-    create(name=name, path=path, tipo='controller', path_placeholder=placeholder_controller)
-    create(name=name, path=path, tipo='binding', path_placeholder=placeholder_binding)
-    create(name=name, path=path, tipo='page', path_placeholder=placeholder_page)
-    sys.exit(0)
-
-
-main()
--- placeholders/lib/src/placeholder_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/instance_manager.dart';
-import 'package:placeholders/src/placeholder_controller.dart';
-
-class PlaceholderBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => PlaceholderController());
-  }
-}
