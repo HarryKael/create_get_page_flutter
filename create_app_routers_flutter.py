d1ef4cddd3c129945185f269956a02567e51c1e3
HarryKael <harrykael1@gmail.com>
2022-11-11 14:13:42 -0400

    Get Arguments
    - Add the shebang to the file.
    - Create the function get_arguments to know if the user send the number
    of arguments that the script needs.
    - We process the arguments to get the name of the pages.

--- create_app_routers_flutter.py
+++ create_app_routers_flutter.py
@@ -1,31 +1,52 @@
+#!/usr/bin/env python3
 import os
 import sys
 
 
 def get_arguments():
-    print(len(sys.argv))
-    # if len(sys.argv) > 1:
-    #     path = sys.argv[1]
-    #     if os.path.exists(path):
-    #         names:list[str] = []
-    #         walk = os.walk(path, topdown=True)
-    #         for minipath in walk:
-    #             splitted = str(minipath[0]).split('/')
-    #             if (len(path.split('/')) + 1) == len(splitted):
-    #                 names.append(splitted[len(splitted) - 1])
-    #         return names
-    #     else:
-    #         print('The path does not exists')
-    #         sys.exit(1)
-    # else:
-    #     print('Introduce the argunemnt.')
-    #     sys.exit(1)
+    """This function collect the names of the files in the path that have the '_page' at the end."""
+    # To know if the number of arguments for the program are the needed.
+    if len(sys.argv) == 3:
+        # Get the path from the arguments
+        path = sys.argv[1]
+        # To know if the path exists.
+        if os.path.exists(path):
+            # The names to return when the function finish.
+            names:list[str] = []
+            # Walk throught the directory to get the name of all pages.
+            walk = os.walk(path, topdown=True)
+            # Verify if the pages pass the conditions to put them into the names.
+            for minipath in walk:
+                # Split the path and see if the path have only 1 directory than the path of the pages.
+                splitted = str(minipath[0]).split('/')
+                if (len(path.split('/')) + 1) == len(splitted):
+                    # folder get the name of the page.
+                    folder = splitted[len(splitted) - 1]
+                    # Split the name into list.
+                    folder_names = folder.split('_')
+                    # If the las word of the name directory is 'page' then we add the name folder without
+                    # the '_page' string to the names list.
+                    if folder_names[len(folder_names) - 1] == 'page':
+                        names.append(folder[:-5])
+            return names
+        else:
+            # If the path does not exists.
+            print('The path does not exists')
+            sys.exit(1)
+    else:
+        # If the number of arguments are not enough
+        print('Introduce all needed the argunemnts.')
+        sys.exit(1)
 
 
 def main():
-    # names = get_arguments()
-    # print(names)
-    pass
+    names = get_arguments()
+    # When there is not pages.
+    if len(names) == 0:
+        print('No names')
+        sys.exit(1)
+    else:
+        print(names)
 
 
 if __name__ == '__main__':
