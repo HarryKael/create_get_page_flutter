4dac798e52a88ded602ed1cee2736a55beef4176
HarryKael <harrykael1@gmail.com>
2022-11-11 16:08:19 -0400

    Create app_routers.dart
    Now the program is working with the new function that create the file
    'app_routers.dart' with the names of the pages.

---
+++ app_routers.dart
@@ -0,0 +1,5 @@
+abstract class AppRouters {
+  static String get home => '/home';
+  static String get listDetail => '/listDetail';
+  static String get partners => '/partners';
+}
--- create_app_routers_flutter.py
+++ create_app_routers_flutter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import sys
+from create_get_page_flutter import renaming
 
 
 def get_arguments():
@@ -28,7 +29,7 @@ def get_arguments():
                     # the '_page' string to the names list.
                     if folder_names[len(folder_names) - 1] == 'page':
                         names.append(folder[:-5])
-            return names
+            return names, sys.argv[2]
         else:
             # If the path does not exists.
             print('The path does not exists')
@@ -39,14 +40,52 @@ def get_arguments():
         sys.exit(1)
 
 
+def create_app_routers(names:list[str], path):
+    """Create the file 'app_routers.dart' with the names of the pages."""
+    app_routers = 'app_routers.dart'
+    # Full path of the file app_routers.dart.
+    full_path = '{}/{}'.format(path, app_routers)
+    # Verify if the directory where the file will be exists.
+    if os.path.exists(path):
+        # first line of the file.
+        first_line = 'abstract class AppRouters {\n'
+        # The lines that the file will have.
+        lines:list[str] = [first_line]
+        # Pass throught the name of the pages.
+        for name in names:
+            # Add a new line to the lines variable.
+            lines.append('  static String get {name} => \'/{name}\';\n'.format(name=renaming(name, version=2)))
+        # Add the last line that the file will have.
+        lines.append('}')
+        # Create the app_routers.dart file.
+        with open(full_path, 'w') as file:
+            # Add all the lines to the file.
+            file.writelines(lines)
+            # Close the file.
+            file.close()
+        # This is when the program finish.
+        print('Finish')
+        sys.exit(0)
+    else:
+        # Print the error.
+        print('Error: This path does not exists (path ubication)')
+        sys.exit(1)
+
+
 def main():
-    names = get_arguments()
+    names, path = get_arguments()
     # When there is not pages.
     if len(names) == 0:
         print('No names')
         sys.exit(1)
     else:
-        print(names)
+        # If the path is equal to nothing.
+        if path == '':
+            # Create the file in the current directory.
+            create_app_routers(names, os.getcwd())
+        else:
+            # Create the file in the path.
+            create_app_routers(names, path)
 
 
 if __name__ == '__main__':
